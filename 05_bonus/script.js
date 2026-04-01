eeec3742a44031a3da3ac262e15e053acf32e418
Giovanni Ghinet <ghinetgiovanni88@gmai.com>
2026-04-01 16:57:04 +0200

    bonus completato

--- 05_bonus/script.js
+++ 05_bonus/script.js
@@ -5,14 +5,33 @@ buon pomeriggio se è pomeriggio (fino alle 17)
 e buonasera se è sera (oltre le 17)
 */
 
-const name = 'Mario';
+const nameProva = 'Mario';
 
 
 // Dichiara la funzione qui.
 
+function SalutoDinamico(nome) {
 
+    const ora = Math.floor(Math.random() * 24);
+
+    let saluto;
+
+    if (ora < 13){
+        saluto = "Buongiorno";
+
+    } else if (ora < 17){
+        saluto = "Buon pomeriggio";
+
+    } else {
+        saluto = "Buonasera";
+
+    }
+
+    return `${saluto} ${nome} ora: ${ora}`;
+    
+}
 // Invoca la funzione qui e stampa il risultato in console
 
-
+console.log(SalutoDinamico(nameProva));
 
 //Risultato atteso se si passa 'Mario' alle 18: // Buonasera Mario.
